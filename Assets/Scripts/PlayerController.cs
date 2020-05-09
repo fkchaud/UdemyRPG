ff35ceecadf9d6a125a0237fa842ec44b110514a
Facundo Chaud <faku_imper@hotmail.com.ar>
2020-05-09 02:44:22 -0300

    Add movement to the player

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour
+{
+    private new Rigidbody2D rigidbody = default;
+
+    [SerializeField]
+    private float moveSpeed = 1f;
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rigidbody.velocity = new Vector2(
+            Input.GetAxisRaw("Horizontal"),
+            Input.GetAxisRaw("Vertical")
+        ) * moveSpeed;
+    }
+}
