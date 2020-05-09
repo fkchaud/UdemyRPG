7a650a8eb7e430b143a8fa33d5d4f6924f585973
Facundo Chaud <faku_imper@hotmail.com.ar>
2020-05-09 16:01:01 -0300

    Camera follows character

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private Transform target = null;
+
+    void Start()
+    {
+        //if (target == null)
+        //    Debug.LogError($"{this.GetType().Name} object has not set target variable.", this);
+        target = PlayerController.Instance.transform;
+    }
+
+    void LateUpdate()
+    {
+        transform.position = new Vector3(target.position.x, target.position.y, transform.position.z);
+    }
+}
