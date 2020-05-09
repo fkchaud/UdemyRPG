478282dd22caa665da158f1668765b73f8dc9bfd
Facundo Chaud <faku_imper@hotmail.com.ar>
2020-05-09 03:31:23 -0300

    Use movement animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,9 +2,11 @@
 using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Animator))]
 public class PlayerController : MonoBehaviour
 {
     private new Rigidbody2D rigidbody = default;
+    private Animator animator = default;
 
     [SerializeField]
     private float moveSpeed = 1f;
@@ -12,14 +14,24 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         rigidbody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        rigidbody.velocity = new Vector2(
-            Input.GetAxisRaw("Horizontal"),
-            Input.GetAxisRaw("Vertical")
-        ) * moveSpeed;
+        float horizontalAxis = Input.GetAxisRaw("Horizontal");
+        float verticalAxis = Input.GetAxisRaw("Vertical");
+
+        rigidbody.velocity = new Vector2(horizontalAxis, verticalAxis) * moveSpeed;
+
+        animator.SetFloat("moveX", horizontalAxis);
+        animator.SetFloat("moveY", verticalAxis);
+
+        if (horizontalAxis == 1 || horizontalAxis == -1 || verticalAxis == 1 || verticalAxis == -1)
+        {
+            animator.SetFloat("lastMoveX", horizontalAxis);
+            animator.SetFloat("lastMoveY", verticalAxis);
+        }
     }
 }
