31b2d159ed346f9d7cb00be6c7c54a6341153c2d
Facundo Chaud <faku_imper@hotmail.com.ar>
2020-05-09 15:49:31 -0300

    Add PlayerLoader for every scene

---
+++ Assets/Scripts/PlayerLoader.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLoader : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject characterPrefab;
+
+    void Awake()
+    {
+        if (PlayerController.Instance == null)
+            Instantiate(characterPrefab);
+    }
+}
